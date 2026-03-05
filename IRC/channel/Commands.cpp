6c7c2278809145bf34e4d2d75d7fe652c2e5a88c
Paul <pribolzi@k2r2p1.42mulhouse.fr>
2026-03-05 14:45:11 +0100

    Reorganisation des commandes

---
+++ IRC/channel/Commands.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Commands.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pribolzi <pribolzi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/05 14:44:24 by pribolzi          #+#    #+#             */
+/*   Updated: 2026/03/05 14:44:40 by pribolzi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Channel.hpp"
+
+void Channel::kick_com(std::string message) {
+
+}
+
+void Channel::invite_com(std::string message) {
+
+}
+
+void Channel::topic_com(std::string message) {
+
+}
+
+void Channel::mode_com(std::string message) {
+
+}
+
+
+void Channel::parse_commands(std::string message) {
+	void (Channel::*ptr[4])(std::string msg) = {&Channel::kick_com , &Channel::invite_com, &Channel::topic_com, &Channel::mode_com};
+	std::string commands[4] = {"KICK", "INVITE", "TOPIC", "MODE"};
+	std::string cmd = message.substr(0, message.find_first_of(" "));
+	for (int i = 0; i < 4; ++i) {
+		if (commands[i].compare(0, std::strlen(commands[i].c_str()), message) == 0) {
+			std::string arg = message.substr(message.find_first_of(" ") + 1, std::strlen(message.c_str()));
+			(this->*ptr[i])(arg);
+		}
+	}
+}
